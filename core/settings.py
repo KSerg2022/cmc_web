2394665f0e840641754a7941b28234bccacdc139
KSerg2022 <koshevoys@gmail.com>
2023-06-07 20:05:25 +0200

    added app account

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',  # my
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
