9b0a42a952a4061c4fdabbe794ab7648f3d2c026
KSerg2022 <koshevoys@gmail.com>
2023-06-08 22:16:03 +0200

    added model 'Cryptocurrency' in cmc

--- cmc/admin.py
+++ cmc/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cryptocurrency
+
+
+@admin.register(Cryptocurrency)
+class CryptocurrencyAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'slug', 'name', 'site', 'contract')
+    list_filter = ('symbol', 'name')
+    search_fields = ('symbol', 'name')
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('name', 'symbol')
+
--- cmc/models.py
+++ cmc/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Cryptocurrency(models.Model):
+    symbol = models.CharField(max_length=25, verbose_name='Symbol')
+    name = models.CharField(max_length=25, verbose_name='Name')
+    slug = models.SlugField(max_length=250)
+    cmc_id = models.IntegerField(verbose_name='ID in coinmarketcap.com', blank=True)
+    site = models.URLField(max_length=200,
+                           verbose_name='Site',
+                           blank=True,
+                           )
+    contract = models.CharField(max_length=255, verbose_name='Contract', blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['symbol']),
+        ]
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}. {self.symbol}'
+
+    # def get_absolute_url(self):
+    #     return reverse('cmc:crypto_detail',
+    #                    args=[self.slug])
+
