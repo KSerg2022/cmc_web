f15aabfbdaeda99a37c7b16886c00ef37e5b4adc
KSerg2022 <koshevoys@gmail.com>
2023-06-15 19:39:35 +0200

    added in model 'Profile' 'validators=[validate_image_file_extension]'

--- account/models.py
+++ account/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.core.validators import validate_image_file_extension
 from django.conf import settings
 
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/',
+                              blank=True,
+                              validators=[validate_image_file_extension]
+                              )
 
     def __str__(self):
         return f'Profile of {self.user.username}'
