6b45feb388caf4f18a375de99a2175ebcb1f312b
KSerg2022 <koshevoys@gmail.com>
2023-06-17 16:47:53 +0200

    refacort some code in some files

--- cmc/utils/load_data_to_db.py
+++ cmc/utils/load_data_to_db.py
@@ -5,18 +5,20 @@ from cmc.handlers.json_file import JsonFile
 from cmc.handlers.cmc_api import Cmc
 
 
-def dump_to_db():
+def dump_to_db(qty):
     filename = Cmc().filename
     currencies = JsonFile().load_data_from_file(filename)
-    # Cryptocurrency.objects.bulk_create(currencies)
-    for currency in currencies:
-        Cryptocurrency.objects.update_or_create(symbol=currency['symbol'],
-                                                name=currency['name'],
-                                                slug=currency['slug'],
-                                                cmc_id=currency['cmc_id'],
+    for currency in currencies[:qty]:
+        Cryptocurrency.objects.update_or_create(symbol=currency['fields']['symbol'],
+                                                name=currency['fields']['name'],
+                                                slug=currency['fields']['slug'],
+                                                website=currency['fields']['slug'],
+                                                contract=currency['fields']['slug'],
+                                                description=currency['fields']['slug'],
+                                                logo=currency['fields']['slug']
                                                 )
-    print('DB was filling')
+    # print('DB was filling')
 
 
 if __name__ == '__main__':
-    dump_to_db()
+    dump_to_db(10)
