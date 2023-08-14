9ad6dede623cdc8f2e04d07464186103c03d6cd5
KSerg2022 <koshevoys@gmail.com>
2023-08-14 21:57:26 +0200

    added 'ProfileSerializer' and list of fields in 'UserSerializer'

--- account/api/cerializers.py
+++ account/api/cerializers.py
@@ -5,14 +5,15 @@ from account.models import Profile
 
 
 # Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        # fields = ['url', 'username', 'email', 'is_staff']
-        fields = '__all__'
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active']
 
 
-class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
