d5b20412fbb450e6e5e2e6ccdaa5a017add2d6c7
KSerg2022 <koshevoys@gmail.com>
2023-08-14 22:03:01 +0200

    added 'ExPortfolioViewSet'

--- exchanger/api/views.py
+++ exchanger/api/views.py
@@ -1,11 +1,17 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 
-from exchanger.models import Exchanger
-from exchanger.api.cerializers import ExchangerSerializer
+from exchanger.models import Exchanger, ExPortfolio
+from exchanger.api.cerializers import ExchangerSerializer, ExPortfolioSerializer
 
 
 class ExchangerViewSet(viewsets.ModelViewSet):
     queryset = Exchanger.objects.all()
     serializer_class = ExchangerSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+class ExPortfolioViewSet(viewsets.ModelViewSet):
+    queryset = ExPortfolio.objects.all()
+    serializer_class = ExPortfolioSerializer
     # permission_classes = [permissions.IsAuthenticated]
