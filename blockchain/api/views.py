724890e8136e3d4cfea1a44ae70eb295265054f5
KSerg2022 <koshevoys@gmail.com>
2023-08-15 13:28:30 +0200

    added 'filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]' in 'BlockchainViewSet' and 'BlockchainPortfolioViewSet'

--- blockchain/api/views.py
+++ blockchain/api/views.py
@@ -1,6 +1,7 @@
-
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from blockchain.models import Blockchain, Portfolio
 from blockchain.api.serializers import BlockchainSerializer, BlockchainPortfolioSerializer
@@ -10,9 +11,17 @@ class BlockchainViewSet(viewsets.ModelViewSet):
     queryset = Blockchain.objects.all()
     serializer_class = BlockchainSerializer
     # permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['name', 'is_active']
+    search_fields = ['name', 'is_active']
+    ordering_fields = ['id', 'name']
 
 
 class BlockchainPortfolioViewSet(viewsets.ModelViewSet):
     queryset = Portfolio.objects.all()
     serializer_class = BlockchainPortfolioSerializer
     # permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['owner__username', 'blockchain__name']
+    search_fields = ['owner__username', 'blockchain__name']
+    ordering_fields = ['owner', 'blockchain__name']
