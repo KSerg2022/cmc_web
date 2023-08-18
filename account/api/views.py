ca987d15213d33bfb29bfc68b1442ec592692a44
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2023-08-18 22:14:41 +0200

    time

--- account/api/views.py
+++ account/api/views.py
@@ -1,9 +1,9 @@
 
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework import permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 from account.models import Profile
 from account.api.serializers import UserSerializer, ProfileSerializer
@@ -12,7 +12,8 @@ from account.api.serializers import UserSerializer, ProfileSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['username', 'is_active']
     search_fields = ['username', 'is_active']
@@ -22,7 +23,8 @@ class UserViewSet(viewsets.ModelViewSet):
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['user__username', 'date_of_birth']
     search_fields = ['user__username', 'date_of_birth']
