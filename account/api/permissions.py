a030cceb3e52ce54e845d5fbc7057a5046390d15
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2023-08-23 16:26:43 +0200

    added permissions - 'IsAuthenticatedAndOwner'

--- account/api/permissions.py
+++ account/api/permissions.py
@@ -1,6 +1,17 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
+class IsAuthenticatedAndOwner(BasePermission):
+    """
+    Allows access only to authenticated users and (owner objects is current user or user is is_staff==True)
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user
+                    and request.user.is_authenticated
+                    and obj.owner == request.user)
+
+
 class IsAuthenticatedAndOwnerOrIsStaff(BasePermission):
     """
     Allows access only to authenticated users and (owner objects is current user or user is is_staff==True)
