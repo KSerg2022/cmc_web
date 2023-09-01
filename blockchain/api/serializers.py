37f2a2415637138b3c222fed5cf07bef6387b69e
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2023-09-01 09:24:52 +0200

    time commit

--- blockchain/api/serializers.py
+++ blockchain/api/serializers.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
 
-
 from blockchain.models import Blockchain, Portfolio
 
 
-# class BlockchainSerializer(serializers.HyperlinkedModelSerializer):
 class BlockchainSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blockchain
@@ -15,4 +13,26 @@ class BlockchainPortfolioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Portfolio
         fields = '__all__'
+        # fields = ['owner', 'blockchain', 'slug', 'wallet', 'comments', 'currencies']
+
+
+# class BlockchainSerializer__(serializers.ModelSerializer):
+#     class Meta:
+#         model = Blockchain
+#         fields = ['id', 'name']
+#
+#
+# class UserSerializer__(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['id', 'username']
+#
+#
+# class BlockchainPortfolioSerializer(serializers.ModelSerializer):
+#     blockchain = BlockchainSerializer__(read_only=True)
+#     owner = UserSerializer__(read_only=True)
+#
+#     class Meta:
+#         model = Portfolio
+#         fields = '__all__'
 
