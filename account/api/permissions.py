3e9bfcc97390dbabc1a710ba4ae25958e2a052bc
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2023-09-02 21:42:51 +0200

    created 'IsAuthenticatedBot' for views_bot

--- account/api/permissions.py
+++ account/api/permissions.py
@@ -23,6 +23,21 @@ class IsAuthenticatedAndOwnerOrIsStaff(BasePermission):
                     and (obj.owner == request.user or request.user.is_staff))
 
 
+class IsAuthenticatedBot(BasePermission):
+    """
+    Allows access only to authenticated users and (owner objects is current user or user is is_staff==True)
+    """
+    def has_permission(self, request, view):
+        # print('+++++++', view.__dict__)
+
+        # user = get_object_or_404(User, profile__telegram=view.kwargs['tel_username'])
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            # user and
+            request.user.profile.telegram == view.kwargs['tel_username'])
+
+
 class IsAuthenticatedAndOwnerOrIsStaff_for_user(BasePermission):
     """
     Allows access only to authenticated users and (current user or user is is_staff==True)
