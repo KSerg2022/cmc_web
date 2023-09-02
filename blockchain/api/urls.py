10e40b491e0042a0dfd7ebf58714c4e990465b90
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2023-09-02 21:42:06 +0200

    created urls for views_bot

--- blockchain/api/urls.py
+++ blockchain/api/urls.py
@@ -2,19 +2,25 @@ from django.urls import path, include
 from rest_framework import routers
 
 
+from blockchain.api.views import BlockchainData
 from blockchain.api.views import BlockchainViewSet, BlockchainPortfolioViewSet, SendEmailData
+from blockchain.api.views_bot import BlockchainDataForBot, BlockchainPortfolioForBot
 
 router = routers.DefaultRouter()
 router.register(r'api/blockchain-portfolio', BlockchainPortfolioViewSet, basename='portfolio')
 router.register(r'api/blockchain', BlockchainViewSet)
 
 
-from blockchain.api.views import BlockchainData
-
 urlpatterns = [
     path('api/blockchain/data/<int:blockchain_id>/', BlockchainData.as_view(),
          name='blockchain-data'),
     path('api/send-email/data/<str:portfolio>/', SendEmailData.as_view(),
          name='send-email-data'),
+
+    path('api/bot/blockchain-portfolio/<str:tel_username>/', BlockchainPortfolioForBot.as_view(),
+         name='portfolio-bot'),
+    path('api/bot/blockchain/data/<int:blockchain_id>/<str:tel_username>/', BlockchainDataForBot.as_view(),
+         name='blockchain-data-bot'),
+
     path('', include(router.urls)),
     ]
