7142c9cc5aa3beb34258c209f7d13225b45025da
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2023-09-12 15:11:10 +0200

    refactored 'permission.py'

--- account/api/permissions.py
+++ account/api/permissions.py
@@ -1,4 +1,8 @@
+from django.contrib.auth.models import User
+
+from rest_framework.authentication import BaseAuthentication
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework import exceptions
 
 
 class IsAuthenticatedAndOwner(BasePermission):
@@ -23,19 +27,26 @@ class IsAuthenticatedAndOwnerOrIsStaff(BasePermission):
                     and (obj.owner == request.user or request.user.is_staff))
 
 
+class IsAuthenticatedAndOwnerOrIsStaff_for_user(BasePermission):
+    """
+    Allows access only to authenticated users and (current user or user is is_staff==True)
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user
+                    and request.user.is_authenticated
+                    and (obj == request.user or request.user.is_staff))
+
+
 class IsAuthenticatedBot(BasePermission):
     """
     Allows access only to authenticated users and (owner objects is current user or user is is_staff==True)
     """
     def has_permission(self, request, view):
-        # print('+++++++', view.__dict__)
-
-        # user = get_object_or_404(User, profile__telegram=view.kwargs['tel_username'])
         return bool(
             request.user and
             request.user.is_authenticated and
-            # user and
-            request.user.profile.telegram == view.kwargs['tel_username'])
+            request.user.profile.telegram == request.META.get('HTTP_TEL_USERNAME'))
 
 
 class UserTelegramAuthentication(BaseAuthentication):
