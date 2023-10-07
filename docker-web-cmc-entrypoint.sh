ef55c5125054b8efdaafc9c001ce22336d021e97
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2023-10-07 13:18:49 +0200

    added load fixtures and create superuser

--- docker-web-cmc-entrypoint.sh
+++ docker-web-cmc-entrypoint.sh
@@ -15,6 +15,22 @@ python manage.py makemigrations && echo "Successful makemigrations" || echo "Not
 echo "Apply database migrations"
 python manage.py migrate && echo "Successful migrate" || echo "Not successful migrate"
 
+# Apply fixtures exchanger
+echo "Apply fixtures exchanger"
+python manage.py loaddata data/fixtures/exchanger.json && echo "Successful loaded exchanger" || echo "Not loaded exchanger"
+
+# Apply fixtures blockchain
+echo "Apply fixtures blockchain"
+python manage.py loaddata data/fixtures/blockchain.json && echo "Successful loaded blockchain" || echo "Not loaded blockchain"
+
+# Apply fixtures cryptocurrency
+echo "Apply fixtures cryptocurrency"
+python manage.py loaddata data/fixtures/cryptocurrency.json && echo "Successful loaded cryptocurrency" || echo "Not loaded cryptocurrency"
+
+# Apply create superuser
+echo "Apply create superuser"
+python manage.py createsuperuser --noinput && echo "Successful created superuser" || echo "Not created superuser"
+
 # Start server
 echo "Starting server"
 gunicorn core.wsgi:application --bind 0.0.0.0:8000
